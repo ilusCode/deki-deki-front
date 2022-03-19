b642e68b0feea5f7df83819b1192513a1a665c02
EDUARDO FERNANDEZ MORALES <edfernandez@externos.santander.com.mx>
2022-03-19 13:07:01 -0600

    Se carga configuracion y paths de trabajo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './pages/default/default.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProgramsComponent } from './pages/programs/programs.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: DefaultComponent
+  },
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path: "programs",
+    component: ProgramsComponent
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DefaultComponent } from './pages/default/default.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProgramsComponent } from './pages/programs/programs.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DefaultComponent,
+    NavbarComponent,
+    ProgramsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
