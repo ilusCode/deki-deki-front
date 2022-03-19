8ff47a8d4d6da3ae9a0ec4a58df50bb73ac85b40
EDUARDO FERNANDEZ MORALES <edfernandez@externos.santander.com.mx>
2022-03-19 13:24:04 -0600

    se eliminan clases no funcionales

--- src/app/services/programs/programs.service.ts
+++ src/app/services/programs/programs.service.ts
@@ -1,27 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Program } from 'src/app/class/program';
-import { UtilsService } from '../utils/utils.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProgramsService {
 
-  constructor(private us: UtilsService) { }
+  constructor() { }
   response: Array<Program> = [];
-
-  getPrograms2(nameFile: string): Array<Program> {
-    this.us.loadFile(nameFile)
-      .subscribe(val => {
-        for (let i = 0; i < val.length; i++) {
-          const element = val[i];
-          let json = JSON.stringify(element);
-          const program: Program = JSON.parse(json);
-          this.response.push(program)
-        }
-      });
-    return this.response;
-  }
 
   getPrograms(db: string) {
     console.log("GET DATA -> " + db);
